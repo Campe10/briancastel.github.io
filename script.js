b695090036b55330db96903c8faf52193cabc321
Campe10 <127438164+Campe10@users.noreply.github.com>
2025-05-29 17:01:17 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarImagen() {
+  const contenedor = document.getElementById("contenedor-imagen");
+  contenedor.innerHTML = '<img src="https://picsum.photos/300" alt="Imagen aleatoria">';
+}
